55554b7b3af592c6e3ad89605bef6c13e7bf5fb2
Blendho <mkpoikannaabasijonah@gmail.com>
2026-04-18 03:07:05 +0100

    Fix email port for production

--- server.js
+++ server.js
@@ -85,7 +85,9 @@ const loginLimiter = rateLimit({
 });
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
