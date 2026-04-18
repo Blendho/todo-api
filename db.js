59d613ffde34778671da6bae390060d55d5175d4
Blendho <mkpoikannaabasijonah@gmail.com>
2026-04-18 04:49:09 +0100

    Fix db connection - use DATABASE_URL

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.DB_URL,
-    ssl: process.env.DB_URL ? { rejectUnauthorized: false } : false
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
