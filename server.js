6496360e928fd8e0b1a1a24f14903c46b2325c5c
Blendho <mkpoikannaabasijonah@gmail.com>
2026-04-18 02:57:24 +0100

    Use dynamic PORT for production

--- server.js
+++ server.js
@@ -624,7 +624,8 @@ app.post('/api/reset-password',
     }
 );
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, function() {
-    console.log('Server running on port 3000');
+app.listen(PORT, function() {
+    console.log(`Server running on port ${PORT}`);
 });
