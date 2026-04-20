892099ddb8890e731cf7f22e042b47f61f049114
Blendho <mkpoikannaabasijonah@gmail.com>
2026-04-20 03:15:27 +0100

    Test PG_CONNECTION variable

--- db.js
+++ db.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
+const connectionString = process.env.PG_CONNECTION || process.env.DB_URL;
+
+console.log('PG_CONNECTION exists:', !!process.env.PG_CONNECTION);
+console.log('DB_URL exists:', !!process.env.DB_URL);
+
 const pool = new Pool({
-    connectionString: process.env.DB_URL,
-    ssl: process.env.DB_URL ? { rejectUnauthorized: false } : false
+    connectionString: connectionString,
+    ssl: connectionString ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
